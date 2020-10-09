16cce3d7de4153f0b51710cf345032ab8e5bd604
Candice Beckford <beckfordcandice@gmail.com>
2020-10-09 12:47:01 -0500

    Exercise 1 complete

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,3 +1,14 @@
-function myFunction(){
-    var x = document.getElementById //trying a ting
+"use strict";
+
+window.onload = function(){
+    
+    console.log('game has loaded');
+    let gameboard = document.querySelectorAll("#board div");
+    for(let i=0; i<gameboard.length; i++){
+        gameboard[i].classList.add('square');
+        //can also try gameboard[i].className="square";
+
+        //Exercise 1
+    };
 }
+
