bdf92d62f60a799116dc36b14b39a9569490fd5e
Candice Beckford <beckfordcandice@gmail.com>
2020-10-10 14:13:23 -0500

    issue with exercise 2; 1, 3 and 6 complete

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,37 +1,7 @@
 "use strict";
 
-window.onload = function(){
+window.onload = function(){   
     
-    console.log('game has loaded');
-    let layout = document.querySelectorAll("#board div");
-    let currentBoard = [];
-    for(let i=0; i<layout.length; i++){
-        layout[i].classList.add('square');
-        //can also try layout[i].className="square";
-
-        //end of Exercise 1
-        layout[i].addEventListener('click', function(){
-            console.log(`You just clicked square ${i}! `);
-        });
-
-        //Exercise 5 - "hover"
-        layout[i].addEventListener('mouseover', function(){
-            this.classList.add('hover');
-        });
-        layout[i].addEventListener('mouseout', function(){
-            this.classList.remove('hover');
-        });
-    };
-     
-    var currentPlayer = document.querySelector("#status");
-    //var value = "x";
-    //switching players
-    currentPlayer.addEventListener('click', function(){
-        currentPlayer= "x";
-        console.log(currentPlayer);
-        //incomplete
-
-    });
     
     /*Winning placements are as follows:
     [0,1,2]
@@ -43,8 +13,108 @@ window.onload = function(){
     [3,4,5]
     [6,7,8]*/
 
+    var winList = [[0,1,2], [0,3,6], [0,4,8], [1,4,7], [2,5,8], [2,4,6], [3,4,5], [6,7,8]]
+    
+    const classX = 'X';
+    const classO = 'O';
+    //let gameOn = true;
+
+    //Exercise 1 - Layout the board
+    let layout = document.querySelectorAll("#board div");
+    let currentPlayer = 'X';
+    for(let i=0; i<layout.length; i++){
+        layout[i].classList.add('square');
+        currentPlayer == classX ? layout[i].classList.add('.square.X') : layout[i].classList.add('.square.O');
+        //can also try layout[i].className="square";
+
+        //test
+        layout[i].addEventListener('click', function(){
+            console.log(`You just clicked square ${i}! `);
+            switchPlayer();
+        });
+
+        //Exercise 3 - "hover" COMPLETE
+        layout[i].addEventListener('mouseover', function(){
+            this.classList.add('hover');
+        });
+        layout[i].addEventListener('mouseout', function(){
+            this.classList.remove('hover');
+        });
+    }
+    console.log('game has loaded');
+
+    //no cheating - click once
+    function noCheat(){
+        console.log("already clicked!");
+    }
+    layout.forEach(cell =>{
+        cell.addEventListener('click', playerTurn, {once: true});
+        //currentPlayer = classX ? square.classList.add('.square.X') : square.classList.add('square.O'); //not working
+    });
+
+    function playerTurn() {
+        return `It's ${currentPlayer}'s turn!`;
+    }
+    function switchPlayer(){
+        currentPlayer = currentPlayer== classX ? classO : classX;
+        console.log(playerTurn());
+    }
+    //DO NOT TOUCH ABOVE UNLESS IT IS A COMMENT!!!
+
+    //switching players
+    let currentBoard = [];
+
+        /*if (currentPlayer == 'x'){
+            player.classList.add('square.X');
+        }
+        else{
+            player.classList.add('.square.O');
+        }*/
+        /*var status = document.querySelector('#status');
+        status.innerHTML= playerTurn();
+    }*/
+     
+    
+
+    //play X or O
+    /*function handleClick(e){
+        const cell = e.target;
+        const currentClass = playO ? classO : classX;
+        noCheat(cell, currentClass);
+    }
+
+    function noCheat(cell, currentClass){
+        cell.classList.add(currentClass);
+        console.log(classX);
+    }*/
+
+    /*empty list used to store gameplay
+    let currentBoard = [];*/
+    
+    
+    /*function playX(ex){
+        ex.className.add("x");
+        ex.innerHTML="X";}*/
+
+        
+    
+
+        /*layout[i].addEventListener('click', function(){
+            playX(this);
+            console.log("clicked");
+        });*/
+
+        /*Exercise 2
+            Add an X or O when clicked
+            alternate X and O
+            add class X or O */
 
 
 
+    /*function playO(oh){
+        oh.className.add("O");
+        oh.textContent="O";
+    }*/
+
+    //function restart(){}
 }
-
